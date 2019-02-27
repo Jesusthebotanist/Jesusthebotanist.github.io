b5cd180e79a54c0a844389ab26b79429de6dda16
Jesus Martinez-Gomez <jmartinezg@berkeley.edu>
2019-02-27 14:54:31 -0500

    updated Matlab code should be good now

--- assets/code/scent/Matlab_NMDS_ANOSIM.m
+++ assets/code/scent/Matlab_NMDS_ANOSIM.m
@@ -1,17 +1,18 @@
-%% *Load Data* 
+%% *NMDS Analysis*
+% NMDS was calculated with both built-in Matlab function 'mdscale' and the
+% Fathom toolkit function 'F_nmds.' (Jones 2015). Similar results were 
+% obtained with both methods and we used 'mdscale' for all 
+% downstream analysis. 
 
-%Clear Enviornment and Read in Data
+% Clear enviornment and read in data
 clear all; close all; clc;
 cd '/Volumes/GoogleDrive/My Drive/Projects/Jesusthebotanist.github.io'; 
 floralData = readtable('assets/code/scent/compounds.csv'); 
 
-%Move to the FathomToolBox folder for relavant functions. 
+% Move to the FathomToolBox folder to call relavant functions. 
 cd ..
 cd '/Volumes/GoogleDrive/My Drive/Projects/Jesusthebotanist.github.io/assets/code/scent/FathomToolBox/'
-%% *NMDS Analysis*
-% NMDS was calculated with both built-in Matlab function 'mdscale' and the
-% Fathom toolkit function 'F_nmds.' (Jones 2015). Similar results were 
-% obtained therefore, we used 'mdscale' for all downstream analysis. 
+
 
 % Extract only scent data
 floralScent = floralData(:,8:end);
@@ -33,7 +34,7 @@ disp ({'Matlab mdscale function 2 Axis Floral stress value =', F_stress});
 disp ({'Fathom f_nmds function 2 Axis Floral stress value =',...
         F_Fathom_nmds.stress});
 %% *nMDS Plot*
-% Generate two dimensional nMDS plot (Figure 2b). 
+% Plot a 2D nNMDS. These points are for Figure 2b 
 
 % 2-Axis nMDS plot with Pollination Syndrome Labels
 figure(1);
@@ -48,7 +49,11 @@ figure(1);
              });      
     gname(table2array(floralData(:,{'Species_Name_Abbreviation'})));
 %% *1- Way ANOSIM* 
-% Calculate 1-way ANOSIM using Fathom Toolbox 'f_anosim' funciton. 
+% ANalysis Of SIMilarity (ANOSIM)is very similar to ANOVA, it is a method 
+% to determine if the means of grouping similar/different, however it is 
+% performed using the disimilarities rather than the raw data. We run 
+% ANOSIM grouping points by species (technical replicates) and by wind and
+% insect pollinated taxa. We only find significance amongest species. 
 
 % ANOSIM - Group by Species
 [F_r,F_p] = f_anosim(F_dissimilaritiesBC,...
@@ -58,3 +63,10 @@ figure(1);
 [FP_r, FP_p] = f_anosim(F_dissimilaritiesBC,...
                (table2array(floralData(:,{'Pollination_Syndrome'}))),...
                1,1000,1);
+           
+%% References
+% Jones DL. 2015. _Fathom Toolbox for Matlab: software for multivariate 
+% ecological and oceanographic data analysis_. College of Marine Science, 
+% University of South Florida, St. Petersburg, FL, USA. 
+% http://www.marine.usf.edu/user/djones/
+
